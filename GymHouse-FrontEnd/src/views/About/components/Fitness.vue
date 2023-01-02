5c927193d2079bf34f499120a98874d35f0951e5
chiwing <chiwingwing@qq.com>
2023-01-02 22:51:18 +0800

    update: Our Contact 部分内容

--- GymHouse-FrontEnd/src/views/About/components/Fitness.vue
+++ GymHouse-FrontEnd/src/views/About/components/Fitness.vue
@@ -4,10 +4,10 @@ import { Ref, ref, reactive } from "vue";
 const dataList = reactive([
   {
     id: 0,
-    state: false,
+    state: true,
     name: "DREAMHOUSE - 同心圆店",
     phone: "020-83465151",
-    img: "https://qbox.youplus.net.cn/about_p4_liuyang.png?imageslim",
+    img: "https://big.justeasy.cn/effect/202101/20210127111104_6010d9c8efba8.png",
     address: "广州市黄埔区同心圆路416号整栋",
   },
   {
@@ -15,7 +15,7 @@ const dataList = reactive([
     state: false,
     name: "DREAMHOUSE - 同乐圆店",
     phone: "020-82413624",
-    img: "https://qbox.youplus.net.cn/about_p4_liuyang.png?imageslim",
+    img: "https://img95.699pic.com/photo/50112/3775.jpg_wh860.jpg",
     address: "广州市黄埔区同乐圆路21号广商购物中心5楼",
   },
   {
@@ -23,20 +23,46 @@ const dataList = reactive([
     state: false,
     name: "DREAMHOUSE - 同德圆店",
     phone: "020-85392403",
-    img: "https://qbox.youplus.net.cn/about_p4_liuyang.png?imageslim",
+    img: "https://5b0988e595225.cdn.sohucs.com/images/20181210/9bbdb69f0e394b379ea2a7da55ee939f.jpeg",
     address: "广州市黄埔区同德圆路416号康达广场3楼",
   },
 ]);
+
+const onMouseEnter: any = (index: number) => {
+  //console.log(index);
+  dataList[0].state = false;
+  for (let i = 0; i < dataList.length; i++) {
+    dataList[i].state = false;
+    if (i == index) {
+      dataList[i].state = true;
+      //console.log(dataList);
+    }
+  }
+};
 </script>
 
 <template>
   <div id="#slide">
     <div class="fitness-box">
       <div class="title"><span>OUR</span> CONTACT</div>
+
       <div class="main-box">
-        <div class="banner"></div>
+        <div class="banner">
+          <img
+            v-for="(item, index) in dataList"
+            :key="index"
+            :src="item.img"
+            v-show="item.state"
+          />
+        </div>
+
         <div class="content">
-          <div class="content-msg" v-for="item in dataList">
+          <div
+            class="content-msg"
+            v-for="(item, index) in dataList"
+            :key="index"
+            @mouseenter="onMouseEnter(index)"
+          >
             <div class="fitness-name">{{ item.name }}</div>
             <div class="fitness-msg">联系电话：{{ item.phone }}</div>
             <div class="fitness-msg">地址：{{ item.address }}</div>
@@ -79,6 +105,13 @@ const dataList = reactive([
     height: 100%;
     background-color: pink;
     margin-right: 30px;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      aspect-ratio: 1 / 1;
+    }
   }
 
   .content {
@@ -93,16 +126,17 @@ const dataList = reactive([
       margin-bottom: 10px;
       border-radius: 5px;
       padding-left: 30px;
+      cursor: pointer;
 
       &:hover {
         border: 2px solid var(--theme-color);
       }
 
-      .fitness-name{
+      .fitness-name {
         margin-top: 25px;
         font-size: 20px;
       }
-      .fitness-msg{
+      .fitness-msg {
         margin-top: 25px;
         font-size: 18px;
       }
