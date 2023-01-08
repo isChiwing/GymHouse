5ad98150088e1dbe30392ae355071eb34b962785
chiwing <chiwingwing@qq.com>
2023-01-08 17:43:55 +0800

    fix: 修复路由跳转不会到顶部问题，引入element

--- GymHouse-FrontEnd/src/main.ts
+++ GymHouse-FrontEnd/src/main.ts
@@ -2,18 +2,26 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { start, close } from "./script/utils/nprogress";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(ElementPlus).mount("#app");
 
 //设置路由导航
 router.beforeEach((to, from, next) => {
   start();
+
+  //设置标题
   if (to.meta.title) {
     document.title = "GymHouse - " + to.meta.title;
   } else {
     document.title = "GymHouse";
   }
 
+  //导航跳转回到顶部
+  document.body.scrollTop = 0; // firefox
+  document.documentElement.scrollTop = 0; // safari
+  window.pageYOffset = 0;
 
   next();
 });
