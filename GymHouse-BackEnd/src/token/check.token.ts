e678cbbd64c6702dbea3438281accc246689e4d6
chiwinghuang <chiwinghuang@trussan.com>
2023-05-07 15:35:01 +0800

    fix: 检查token修改

--- GymHouse-BackEnd/src/token/check.token.ts
+++ GymHouse-BackEnd/src/token/check.token.ts
@@ -1,21 +1,27 @@
-
 const checkJwt = require('jsonwebtoken');
 const util = require('util')
 const verify = util.promisify(checkJwt.verify) // 解密
 import tokenConfig from '../config/token.config'
 const {secret} = tokenConfig
 //检查token是否过期
-module.exports = async (token:any) =>{
-  if (token) {
-	let payload
-	try{
-	  payload = await verify(token, secret)  // // 解密，获取payload
-	  console.log(payload,"payload")
-	}catch (err){
-	  return false
-	}
-	return true
-  } else {
-	return false;
+
+interface DecodedToken {
+	userId: number;
+	userName: string;
+	phone:String;
+	userType: number;
+	iat: number;
+	exp: number;
   }
-};
+  
+  async function decodeToken(token: string):  Promise<DecodedToken | null> {
+	try {
+	  const decoded = await checkJwt.verify(token, secret) as DecodedToken; // 解码并验证JWT token
+	  return decoded; // 返回解码后的JWT token
+	} catch (err) {
+	  console.error(err);
+	  return null;
+	}
+  }
+
+  export default decodeToken;
--- GymHouse-BackEnd/src/token/create.token.ts
+++ GymHouse-BackEnd/src/token/create.token.ts
@@ -7,7 +7,7 @@ module.exports = function(userId:number,userName:String,phone:String,userType:nu
   //第一个参数是载荷，用于编码后存储在 token 中的数据
   //第二个是密钥，自己定义的，验证的时候也是要相同的密钥才能解码
   //第三个是options，可以设置 token 的过期时间
-  const token = jwt.sign({userId:userId,userName: userName,phone:phone,userType:userType}, secret, {expiresIn: '1m'});
+  const token = jwt.sign({userId:userId,userName: userName,phone:phone,userType:userType}, secret, {expiresIn: '1h'});
   console.log(token)
   return token;
 };
